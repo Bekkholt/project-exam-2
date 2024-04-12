15a1cc38869b0526df694dd24b349e993a02464e
Tina <tina.bekkholt@hotmail.com>
2024-04-12 15:08:52 +0200

    Fixed classname and fill errors

--- src/Components/Icons/Profile/Login/index.jsx
+++ src/Components/Icons/Profile/Login/index.jsx
@@ -7,14 +7,11 @@ export default function Loginicon() {
       width="25"
       height="25"
       fill="currentColor"
-      class="bi bi-person-plus-fill"
+      className="bi bi-person-plus-fill"
       viewBox="0 0 16 16"
     >
       <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
-      <path
-        fill-rule="evenodd"
-        d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"
-      />
+      <path d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5" />
     </S.Login>
   );
 }
