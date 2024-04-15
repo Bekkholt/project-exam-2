418e091c436aadcad9101d584a9e0d3e27a556bb
Tina <tina.bekkholt@hotmail.com>
2024-04-15 16:02:42 +0200

    homepage in layout

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,18 +1,20 @@
 import * as S from "./index.styles";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
+import HomePage from "../../Pages/Homepage";
 
 export default function Layout() {
   return (
     <S.Pagewrapper>
       <Header />
-      <Routes>
+      {/* <Routes>
         <Route index />
         <Route index />
         <Route index />
         <Route index />
-      </Routes>
+      </Routes> */}
+      <HomePage />
       <S.Body />
       <Footer />
     </S.Pagewrapper>
