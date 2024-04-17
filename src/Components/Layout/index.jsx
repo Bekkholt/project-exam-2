2e5f72ec344fc145c5f267977fe6c3d1aac9c46d
Tina <tina.bekkholt@hotmail.com>
2024-04-17 13:36:17 +0200

    added venuepage and links to layour

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,20 +1,20 @@
 import * as S from "./index.styles";
-// import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
 import HomePage from "../../Pages/Homepage";
+import Homeicon from "../Icons/Home";
+import Venuepage from "../../Pages/Venuepage";
 
 export default function Layout() {
   return (
     <S.Pagewrapper>
       <Header />
-      {/* <Routes>
-        <Route index />
-        <Route index />
-        <Route index />
-        <Route index />
-      </Routes> */}
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route index element={<Homeicon />} />
+        <Route path="/Pages/Venuepage/:id" element={<Venuepage />} />
+      </Routes>
       <S.Body />
       <Footer />
     </S.Pagewrapper>
