a14f8b76c1a4cd170aae1ae9ba00608c2dcbb88f
Tina <tina.bekkholt@hotmail.com>
2024-04-18 13:29:10 +0200

    create component

---
+++ src/Components/Searchbar/index.jsx
@@ -0,0 +1,51 @@
+import * as S from "./index.styles";
+import FetchAPI from "../../Hooks/API";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Searchbar() {
+  const [SearchInput, setSearchInput] = useState("");
+  const [SearchResults, setSearchResults] = useState("");
+  const { venues } = FetchAPI("https://v2.api.noroff.dev/holidaze/venues/");
+
+  useEffect(() => {
+    if (SearchInput.trim() === "") {
+      setSearchResults([]);
+      return;
+    }
+
+    const results = venues.filter((venue) => {
+      return venue.name.toLowerCase().includes(SearchInput.toLocaleLowerCase());
+    });
+    setSearchResults(results);
+  }, [venues, SearchInput]);
+
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <S.Searchdiv>
+      <S.Searchbar
+        className="text"
+        onSubmit={(e) => e.preventDefault()}
+        action="search"
+        type="text"
+        placeholder="Search venues"
+        value={SearchInput}
+        onChange={HandleSearch}
+      ></S.Searchbar>
+      <S.SearchItems>
+        {SearchResults.length === 0 && SearchInput.trim() !== " "
+          ? " "
+          : SearchResults.map((venue) => (
+              <Link to={`../../Pages/Venuepage/${venue.id}`} key={venue.id}>
+                <S.SearchResults className="text">{venue.name}</S.SearchResults>
+              </Link>
+            ))}
+      </S.SearchItems>
+    </S.Searchdiv>
+  );
+}
