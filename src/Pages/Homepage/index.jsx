29f840cf944e1e9e8be807f49c1bad9bb5bbb5cc
Tina <tina.bekkholt@hotmail.com>
2024-04-18 13:29:36 +0200

    add to homepage

--- src/Pages/Homepage/index.jsx
+++ src/Pages/Homepage/index.jsx
@@ -1,9 +1,11 @@
+import Searchbar from "../../Components/Searchbar";
 import VenueList from "../../Components/Venuelist";
 import * as S from "./index.styles";
 
 export default function HomePage() {
   return (
     <div>
+      <Searchbar />
       <S.Cards>
         <VenueList />
       </S.Cards>
