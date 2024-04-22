08806252f1c17c5af54a631a3054830382452470
Tina <tina.bekkholt@hotmail.com>
2024-04-22 13:07:38 +0200

    show bookings

--- src/Components/Calendar/index.jsx
+++ src/Components/Calendar/index.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Calendar from "@demark-pro/react-booking-calendar";
 
-const reserved = [
-  {
-    startDate: new Date(2023, 3, 22),
-    endDate: new Date(2016, 4, 0o5),
-  },
-];
-
 const MyBookingCalendar = () => {
-  const [selectedDates, setSelectedDates] = useState([]);
-  const handleChange = (e) => setSelectedDates(e);
+  const [bookingDetails, setBookingDetails] = useState();
+  let { id } = useParams();
+
+  useEffect(() => {
+    async function getData(url) {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setBookingDetails(json.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData(`https://v2.api.noroff.dev/holidaze/venues/${id}?_bookings=true`);
+  }, [id]);
+
+  if (!bookingDetails) return <div></div>;
+
+  const reserved = bookingDetails.bookings.map((booking) => {
+    return {
+      startDate: new Date(booking.dateFrom),
+      endDate: new Date(booking.dateTo),
+    };
+  });
 
   return (
     <Calendar
-      selected={selectedDates}
-      onChange={handleChange}
-      onOverbook={(e, err) => alert(err)}
       disabled={(date, state) => !state.isSameMonth}
       reserved={reserved}
       variant="events"
