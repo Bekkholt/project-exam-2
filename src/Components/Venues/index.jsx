8b35bc5f5c18852ec62d0facc4c4390dccf7606f
Tina <tina.bekkholt@hotmail.com>
2024-04-28 18:31:32 +0200

    if image fixed

--- src/Components/Venues/index.jsx
+++ src/Components/Venues/index.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router-dom";
 import * as S from "./index.styles";
 
 export default function Venues(venue) {
+  function Image() {
+    if (venue?.image[0]?.url === undefined) {
+      return <S.Title>No image</S.Title>;
+    } else {
+      return <S.VenueImage src={venue.image[0].url} />;
+    }
+  }
   return (
     <S.VenueCard>
       <Link to={`../../Pages/Venuepage/${venue.id}`}>
-        <S.TopCard>
-          <S.VenueImage src={venue.image[0].url} />
-        </S.TopCard>
+        <S.TopCard>{<Image />}</S.TopCard>
         <S.Title className="header">{venue.name}</S.Title>
         <S.VenueDescription className="text">
           {venue.location.city}, {venue.location.country}
