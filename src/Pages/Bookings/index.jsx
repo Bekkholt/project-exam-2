04fd1aece922965948f048844e9c056044fdafd9
Tina <tina.bekkholt@hotmail.com>
2024-05-02 11:59:25 +0200

    small fix booking

--- src/Pages/Bookings/index.jsx
+++ src/Pages/Bookings/index.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 import * as S from "./index.styles";
 
 const name = localStorage.getItem("name");
-// const token = localStorage.getItem("accessToken");
 
 export default function Bookings() {
   const bookings = FetchMyBookings(
     "https://v2.api.noroff.dev/holidaze/profiles/" + name + "?_bookings=true"
   );
   console.log(bookings);
+  if (bookings.isLoading === true) return <></>;
 
   if (bookings.isError === true) {
     return (
@@ -25,47 +25,45 @@ export default function Bookings() {
         </S.VenueCard>
       </S.ProductWrapper>
     );
+  } else {
+    return (
+      <S.OuterDiv>
+        {bookings.bookings.map((booking) => {
+          return (
+            <S.VenueCard key={booking.id}>
+              <Link to={`../../Pages/Venuepage/${booking.venue.id}`}>
+                <S.TopCard>
+                  <S.VenueImage src={booking.venue.media.url} />
+                </S.TopCard>
+                <S.Title className="header">{booking.venue.name}</S.Title>
+                <S.VenueDescription className="text">
+                  {booking.venue.location.city},{" "}
+                  {booking.venue.location.country}
+                </S.VenueDescription>
+                <S.VenueDescription>
+                  {booking.venue.description}
+                </S.VenueDescription>
+                <S.VenueDescription className="text">
+                  Booked dates:{" "}
+                </S.VenueDescription>
+                <S.VenueDescription className="text">
+                  {booking.dateFrom}{" "}
+                </S.VenueDescription>{" "}
+                <S.VenueDescription className="text"> to </S.VenueDescription>{" "}
+                <S.VenueDescription className="text">
+                  {booking.dateTo}
+                </S.VenueDescription>
+                <S.BottomCard>
+                  <S.VenuePrice className="text">
+                    ${booking.venue.price}/night
+                  </S.VenuePrice>
+                </S.BottomCard>
+              </Link>
+            </S.VenueCard>
+          );
+        })}
+        <Logout />
+      </S.OuterDiv>
+    );
   }
-
-  if (bookings.bookings.length === 0) return <></>;
-  if (bookings.isLoading === true) return <></>;
-
-  return (
-    <S.OuterDiv>
-      {bookings.bookings.map((booking) => {
-        return (
-          <S.VenueCard key={booking.id}>
-            <Link to={`../../Pages/Venuepage/${booking.venue.id}`}>
-              <S.TopCard>
-                <S.VenueImage src={booking.venue.media.url} />
-              </S.TopCard>
-              <S.Title className="header">{booking.venue.name}</S.Title>
-              <S.VenueDescription className="text">
-                {booking.venue.location.city}, {booking.venue.location.country}
-              </S.VenueDescription>
-              <S.VenueDescription>
-                {booking.venue.description}
-              </S.VenueDescription>
-              <S.VenueDescription className="text">
-                Booked dates:{" "}
-              </S.VenueDescription>
-              <S.VenueDescription className="text">
-                {booking.dateFrom}{" "}
-              </S.VenueDescription>{" "}
-              <S.VenueDescription className="text"> to </S.VenueDescription>{" "}
-              <S.VenueDescription className="text">
-                {booking.dateTo}
-              </S.VenueDescription>
-              <S.BottomCard>
-                <S.VenuePrice className="text">
-                  ${booking.venue.price}/night
-                </S.VenuePrice>
-              </S.BottomCard>
-            </Link>
-          </S.VenueCard>
-        );
-      })}
-      <Logout />
-    </S.OuterDiv>
-  );
 }
