4d27f97eea6e4fbb2abc4e03e1ed798c2ae8ddcc
Tina <tina.bekkholt@hotmail.com>
2024-05-02 13:16:03 +0200

    add profilepage to header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,6 +5,24 @@ import Loginicon from "../Icons/Profile/Login";
 import { Link } from "react-router-dom";
 import * as S from "./index.styles";
 
+const token = localStorage.getItem("accessToken");
+
+function LoggedIn() {
+  if (token !== undefined) {
+    return (
+      <Link to="../../Pages/Profilepage">
+        <Loginicon />
+      </Link>
+    );
+  } else {
+    return (
+      <Link to="../../Pages/Loginpage">
+        <Loginicon />
+      </Link>
+    );
+  }
+}
+
 export default function Header() {
   return (
     <S.Header>
@@ -22,9 +40,7 @@ export default function Header() {
         <Link to="../../Pages/Bookings">
           <Calendaricon />
         </Link>
-        <Link to="../../Pages/Loginpage">
-          <Loginicon />
-        </Link>
+        {LoggedIn()}
       </S.Right>
     </S.Header>
   );
