c6fc7fe1673c9eae14842df5f3b35504775a174d
Tina <tina.bekkholt@hotmail.com>
2024-05-15 15:03:41 +0200

    added first styling

--- src/Components/UpdateVenue/index.jsx
+++ src/Components/UpdateVenue/index.jsx
@@ -1,3 +1,53 @@
+import * as S from "./index.styles";
+import { Link } from "react-router-dom";
+
 export default function UpdateVenue() {
-  return <div></div>;
+  return (
+    <S.Wrapper>
+      <S.VenueCard>
+        <S.TopCard>
+          <S.Title className="header">Update venue</S.Title>
+        </S.TopCard>
+        <S.InsertDiv>
+          <S.Insert placeholder="Image url" type="url" />
+          <S.Insert placeholder="Venue name*" />
+          <S.Insert placeholder="Address" />
+          <S.Insert placeholder="City" />
+          <S.Insert placeholder="Zip" />
+          <S.Insert placeholder="Country" />
+          <S.DescriptionInsert placeholder="Description*" type="text" />
+          <S.Insert placeholder="Price pr/night*" type="number" />
+          <S.Insert placeholder="Max guests*" type="number" />
+        </S.InsertDiv>
+        <S.VenueDescription className="text"></S.VenueDescription>
+        <S.InsertDiv>
+          <S.CheckboxDiv>
+            <S.Insert type="checkbox" />
+            <S.VenueDescription>Wifi</S.VenueDescription>
+          </S.CheckboxDiv>
+          <S.CheckboxDiv>
+            <S.Insert type="checkbox" />
+            <S.VenueDescription>Parking</S.VenueDescription>
+          </S.CheckboxDiv>
+          <S.CheckboxDiv>
+            <S.Insert type="checkbox" />
+            <S.VenueDescription>Breakfast</S.VenueDescription>
+          </S.CheckboxDiv>
+          <S.CheckboxDiv>
+            <S.Insert type="checkbox" />
+            <S.VenueDescription>Pets</S.VenueDescription>
+          </S.CheckboxDiv>
+        </S.InsertDiv>
+        <S.BottomCard>
+          <S.InsertDiv>
+            <S.Button className="header">Update venue</S.Button>
+            <S.Button className="header">Delete venue</S.Button>
+            <Link to={"../../Pages/Profilepage"}>
+              <S.Button className="header">Go back</S.Button>
+            </Link>
+          </S.InsertDiv>
+        </S.BottomCard>
+      </S.VenueCard>
+    </S.Wrapper>
+  );
 }
---
+++ src/Components/UpdateVenue/index.styles.jsx
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10%;
+`;
+
+export const VenueCard = styled.form`
+  background-color: #172625;
+  width: 55vh;
+  margin: 2%;
+  margin-top: 5%;
+  border-radius: 1em;
+  box-shadow: 2px 2px 2px 2px rgb(0 0 0 / 20%);
+  }
+`;
+
+export const InsertDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const VenueRating = styled.p`
+  color: #f29c6b;
+`;
+
+export const Title = styled.h1`
+  color: #f29c6b;
+`;
+
+export const VenueImage = styled.img`
+  width: 100%;
+  border-radius: 1em;
+`;
+
+export const VenueDescription = styled.p`
+  color: #f29c6b;
+  padding-left: 5%;
+`;
+
+export const Button = styled.button`
+  background-color: #f29c6b;
+  border-radius: 0.5rem;
+  margin: 2%;
+  width: 10rem;
+  height: 3rem;
+  cursor: pointer;
+
+  :hover {
+    background-color: #f6813e;
+  }
+`;
+
+export const TopCard = styled.div`
+  display: flex;
+  padding: 3%;
+  justify-content: center;
+`;
+
+export const BottomCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 3%;
+`;
+
+export const Insert = styled.input`
+  background-color: #d9b6a3;
+  height: 23px;
+  max-width: 30vh;
+  border-radius: 0.5rem;
+  margin-top: 3%;
+`;
+
+export const DescriptionInsert = styled.input`
+  background-color: #d9b6a3;
+  height: 23px;
+  max-width: 30vh;
+  border-radius: 0.5rem;
+  margin-top: 10%;
+`;
+
+export const CheckboxDiv = styled.div`
+  display: flex;
+  align-items: center;
+`;
