de796b9a9d15e414b4610e02ab6d4b348b0741bc
Tina <tina.bekkholt@hotmail.com>
2024-05-18 15:18:40 +0200

    profile page fixed

--- src/Pages/Profilepage/index.jsx
+++ src/Pages/Profilepage/index.jsx
@@ -1,27 +1,62 @@
 import Logout from "../../Components/Logout";
 import { Link } from "react-router-dom";
 import * as S from "./index.styles";
+import FetchMyProfile from "../../Hooks/MyProfileAPI";
+import { BounceLoader } from "react-spinners";
 
 export default function ProfilePage() {
+  const name = localStorage.getItem("name");
+
+  const { profile, isLoading } = FetchMyProfile(
+    `https://v2.api.noroff.dev/holidaze/profiles/${name}`
+  );
+
+  function Content() {
+    if (isLoading === true) {
+      return (
+        <BounceLoader
+          loading={isLoading}
+          size={30}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          color="#f29c6b"
+        />
+      );
+    }
+    if (!profile) {
+      return <></>;
+    }
+    return (
+      <>
+        <S.TopCard>
+          <S.VenueImage
+            alt={profile.avatar.alt}
+            src={profile.avatar.url}
+          ></S.VenueImage>
+          <S.VenueDescription className="text">
+            {profile.bio}
+          </S.VenueDescription>
+        </S.TopCard>
+        <S.VenueTitle className="header">{profile.name}</S.VenueTitle>
+        <S.VenueDescription>
+          <Link to={"../../Pages/Updateprofilepage"}>
+            <S.BackButton className="text">Update profile</S.BackButton>
+          </Link>
+        </S.VenueDescription>
+        <Link to={"../../Pages/Createvenuepage"}>
+          <S.BackButton className="text">Add venue</S.BackButton>
+        </Link>
+        <S.ButtonDiv>
+          <Logout />
+        </S.ButtonDiv>
+      </>
+    );
+  }
   return (
     <div>
       <S.Wrapper>
         <S.CardDiv>
-          <S.TopCard>
-            <S.VenueImage></S.VenueImage>
-            <S.VenueDescription>
-              <Link to={"../../Pages/Updateprofilepage"}>
-                <S.BackButton className="text">Update profile</S.BackButton>
-              </Link>
-            </S.VenueDescription>
-          </S.TopCard>
-          <S.VenueTitle className="header">Profile name</S.VenueTitle>
-          <Link to={"../../Pages/Createvenuepage"}>
-            <S.BackButton className="text">Add venue</S.BackButton>
-          </Link>
-          <S.ButtonDiv>
-            <Logout />
-          </S.ButtonDiv>
+          <Content />
         </S.CardDiv>
       </S.Wrapper>
     </div>
