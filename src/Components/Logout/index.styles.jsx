5c5fbdf4d4c3ae1fbf668e0278b57f3536444672
Tina <tina.bekkholt@hotmail.com>
2024-05-24 10:01:33 +0200

    logout stylings removed

--- src/Components/Logout/index.styles.jsx
+++ src/Components/Logout/index.styles.jsx
@@ -1,27 +1,5 @@
 import styled from "styled-components";
 
-export const OuterDiv = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-export const ProfileDiv = styled.div`
-  background-color: #172625d8;
-  margin-top: 10%;
-  border-radius: 1em;
-  display: flex;
-  justify-content: center;
-  width: 50vh;
-  height: 70vh;
-  box-shadow: 2px 2px 2px 2px rgb(0 0 0 / 20%);
-`;
-
-export const Title = styled.h1`
-  color: #f29c6b;
-  display: flex;
-  justify-content: center;
-`;
-
 export const ProfileDetails = styled.form`
   display: flex;
   flex-direction: column;
