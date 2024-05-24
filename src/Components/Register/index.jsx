8ba32a874760ec77b6118309b1353d7f708e997a
Tina <tina.bekkholt@hotmail.com>
2024-05-24 12:46:12 +0200

    navigate register

--- src/Components/Register/index.jsx
+++ src/Components/Register/index.jsx
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import * as S from "./index.styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const URL = "https://v2.api.noroff.dev/auth/register";
 
@@ -31,6 +31,8 @@ const schema = yup
   .required();
 
 export default function CreateAccount() {
+  const navigate = useNavigate();
+
   const { register } = useForm({
     resolver: yupResolver(schema),
   });
@@ -72,6 +74,7 @@ export default function CreateAccount() {
       console.log(json);
       if (response.status === 201) {
         alert(`Account created`);
+        navigate("../../Pages/Loginpage");
       } else {
         alert(`Something went wrong. Statuscode: ` + response.status);
       }
